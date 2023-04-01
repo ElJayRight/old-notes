b82b3cf6e26e98ecb81b27837a42b5e6af7e838c
eljay <liamwri2003@hotmail.com>
2023-04-02 02:33:37 +1100

    automation...

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
 import glob
 # root_dir needs a trailing slash (i.e. /root/dir/)
 for filename in glob.iglob('./'+'**/*.md', recursive=True):
-     print(f'* [{filename.split("/")[-1][:-3]}]({filename.replace(" ","%20").split("/")[-1]})')
+     print(f'* [{filename.split("/")[-1][:-3]}](CTFs/PicoCTF23/{filename.replace(" ","%20").split("/")[-1]})')
